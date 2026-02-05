cc5c9d2fb595c29ae22a62b4d7ce1bde7d8a1a89
Jojo-brz <jlucas.gferrira@gmail.com>
2026-02-05 16:48:03 -0300

    fix: ajusta configuração do TypeScript e modifica script de build para ignorar lint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next"; // Pode manter o import, não atrapalha
+import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
@@ -12,19 +12,14 @@ const withPWA = withPWAInit({
   },
 });
 
-// MUDANÇA AQUI: Tirei o ": NextConfig" e deixei sem tipo (ou poderia ser : any)
-// Isso faz o TypeScript parar de reclamar do 'eslint'
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   images: {
     unoptimized: true,
   },
 };
 
 export default withPWA(nextConfig as NextConfig);
-// O 'as NextConfig' no final garante que o withPWA receba o que espera
